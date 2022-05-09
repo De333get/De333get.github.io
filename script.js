61a750c57a3fa6bb03343d172e7bf5894fa0f1bf
Dzianis Abushkevich <101499613+De333get@users.noreply.github.com>
2022-05-09 11:30:06 +0300

    feat: add files in repository

---
+++ script.js
@@ -0,0 +1,220 @@
+document.head.innerHTML += '<link rel="stylesheet" href="style.css">'
+
+
+let div = document.createElement('div')
+document.body.append(div)
+div.className = "conteiner"
+
+let str = document.createElement('h1')
+document.body.append(str)
+str.className = 'stroke'
+str.innerHTML = 'Клавиатура создана в операционной системе \'Windows\'.<br> Для переключения раскладки нажмите \'ALt + Shift\''
+
+let textarea = document.createElement('textarea')
+div.append(textarea)
+textarea.className = 'text'
+let del = document.createElement('div')
+div.append(del)
+del.className = 'del'
+del.classList.add('keys')
+del.innerHTML = 'Del'
+
+
+let KeybordWrapper = document.createElement('div')
+div.append(KeybordWrapper)
+KeybordWrapper.className = 'keyboard__wrapper'
+
+let row1 = document.createElement('div');
+let row2 = document.createElement('div');
+let row3 = document.createElement('div');
+let row4 = document.createElement('div');
+let row5 = document.createElement('div')
+let rowArr = [row1, row2, row3, row4, row5];
+rowArr.forEach(el => el.classList.add('row'));
+
+KeybordWrapper.append(...rowArr)
+
+let firstKey = document.createElement('div')
+row1.prepend(firstKey)
+firstKey.className = 'first_key'
+firstKey.classList.add('keys')
+firstKey.innerHTML = '`'
+
+let tab = document.createElement('div')
+row2.append(tab)
+tab.className = 'tab_key'
+tab.classList.add('keys')
+tab.innerHTML = 'Tab'
+
+let capslock = document.createElement('div')
+row3.prepend(capslock)
+capslock.className = 'capslock_key'
+capslock.classList.add('keys')
+capslock.innerHTML = 'Capslock'
+
+let shiftLeft = document.createElement('div')
+row4.prepend(shiftLeft)
+shiftLeft.className = 'shift_left'
+shiftLeft.classList.add('keys')
+shiftLeft.innerHTML = 'Shift'
+
+let CtrlLeft = document.createElement('div')
+row5.append(CtrlLeft)
+CtrlLeft.className = 'keys'
+CtrlLeft.classList.add('ctrl')
+CtrlLeft.innerHTML = 'Ctrl'
+
+let fn = document.createElement('div')
+row5.append(fn)
+fn.className = 'keys'
+fn.classList.add('fn')
+fn.innerHTML = 'Fn'
+
+let Win = document.createElement('div')
+row5.append(Win)
+Win.className = 'keys'
+Win.classList.add('win')
+Win.innerHTML = 'Win'
+
+let altLeft = document.createElement('div')
+row5.append(altLeft)
+altLeft.className = 'keys'
+altLeft.classList.add('alt')
+altLeft.innerHTML = 'Alt'
+
+let space = document.createElement('div')
+row5.append(space)
+space.className = 'keys'
+space.classList.add('space_key')
+space.innerHTML = ''
+
+let altRight = document.createElement('div')
+row5.append(altRight)
+altRight.className = 'keys'
+altRight.classList.add('alt')
+altRight.innerHTML = 'Alt'
+
+let ctrlRight = document.createElement('div')
+row5.append(ctrlRight)
+ctrlRight.className = 'keys'
+ctrlRight.classList.add('ctrl')
+ctrlRight.innerHTML = 'Ctrl'
+
+let leftArrow = document.createElement('div')
+row5.append(leftArrow)
+leftArrow.className = 'keys'
+leftArrow.classList.add('left_arr')
+leftArrow.innerHTML = '&larr;'
+
+let upDown = document.createElement('div')
+row5.append(upDown)
+let up = document.createElement('div')
+upDown.append(up)
+up.innerHTML = '&uarr;'
+up.classList.add('keys', 'up_down', 'up')
+
+
+let down = document.createElement('div')
+upDown.append(down)
+down.innerHTML = '&darr;'
+down.className = 'up_down'
+down.classList.add('keys', 'down')
+
+let leftRight = document.createElement('div')
+row5.append(leftRight)
+leftRight.className = 'keys'
+leftRight.classList.add('right_arr')
+leftRight.innerHTML = '&rarr;'
+
+
+
+
+
+let keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+	'-', '=', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
+	'O', 'P', '[', ']', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
+	'L', ';', '\'', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/']
+
+let keysRU = ['Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш',
+	'Щ', 'З', 'Х', 'Ъ', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л',
+	'Д', 'Ж', 'Э', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.']
+for (let i = 0; i < keys.length; i++) {
+	if (i < 12) {
+		let key = document.createElement('div')
+		row1.append(key)
+		key.className = 'keys'
+		key.classList.add('ru')
+		key.innerHTML = keysRU[i]
+	}
+	if (i >= 12 && i < 24) {
+		let key = document.createElement('div')
+		row2.append(key)
+		key.className = 'keys'
+		key.classList.add('ru')
+		key.innerHTML = keysRU[i]
+	}
+	if (i >= 24 && i < 35) {
+		let key = document.createElement('div')
+		row3.append(key)
+		key.className = 'keys'
+		key.classList.add('ru')
+		key.innerHTML = keysRU[i]
+	}
+	if (i >= 35 && i < 46) {
+		let key = document.createElement('div')
+		row4.append(key)
+		key.className = 'keys'
+		key.classList.add('ru')
+		key.innerHTML = keysRU[i]
+	}
+}
+
+for (let i = 0; i < keys.length; i++) {
+	if (i < 12) {
+		let key = document.createElement('div')
+		row1.append(key)
+		key.className = 'keys'
+		key.innerHTML = keys[i]
+	}
+	if (i >= 12 && i < 24) {
+		let key = document.createElement('div')
+		row2.append(key)
+		key.className = 'keys'
+		key.innerHTML = keys[i]
+	}
+	if (i >= 24 && i < 35) {
+		let key = document.createElement('div')
+		row3.append(key)
+		key.className = 'keys'
+		key.innerHTML = keys[i]
+	}
+	if (i >= 35 && i < 46) {
+		let key = document.createElement('div')
+		row4.append(key)
+		key.className = 'keys'
+		key.innerHTML = keys[i]
+	}
+}
+let slash = document.createElement('div')
+row2.append(slash)
+slash.className = 'slash_key'
+slash.classList.add('keys')
+slash.innerHTML = ' \\ '
+
+let Backspace = document.createElement('div')
+row1.append(Backspace)
+Backspace.className = 'backspace_key'
+Backspace.classList.add('keys')
+Backspace.innerHTML = 'Backspace'
+
+let enter = document.createElement('div')
+row3.append(enter)
+enter.className = 'enter_key'
+enter.classList.add('keys')
+enter.innerHTML = 'Enter'
+
+let shiftright = document.createElement('div')
+row4.append(shiftright)
+shiftright.className = 'shift_right'
+shiftright.classList.add('keys')
+shiftright.innerHTML = 'Shift'
---
+++ window.js
@@ -0,0 +1,340 @@
+let button = document.querySelectorAll('.keys')
+let textInput = document.querySelector('.text')
+let KeyPad = document.querySelector('.keyboard__wrapper')
+if (button && textInput && KeyPad) {
+	let capsLockMode = false
+	button.forEach(key => {
+		key.addEventListener('click', function () {
+			if (this.classList.contains('capslock_key')) {
+				this.classList.toggle('active_caps')
+				capsLockMode ? capsLockMode = false : capsLockMode = true
+			} else if (this.classList.contains('backspace_key')) {
+				let str = textInput.innerHTML
+				textInput.innerHTML = str.substring(0, (str.length - 1))
+			} else if (this.classList.contains('space_key')) {
+				textInput.innerHTML += ' '
+			} else if (this.classList.contains('tab_key')) {
+				textInput.innerHTML += '\t'
+			} else if (this.classList.contains('ctrl')) {
+				textInput.innerHTML += ''
+			} else if (this.classList.contains('alt')) {
+				textInput.innerHTML += ''
+			} else if (this.classList.contains('win')) {
+				textInput.innerHTML += ''
+			} else if (this.classList.contains('fn')) {
+				textInput.innerHTML += ''
+			} else if (this.classList.contains('shift_left') || this.classList.contains('shift_right')) {
+				textInput.innerHTML += ''
+			} else if (this.classList.contains('enter_key')) {
+				textInput.innerHTML += '\n'
+			} else {
+				if (capsLockMode) {
+					textInput.innerHTML += this.innerText.toUpperCase()
+				} else {
+					textInput.innerHTML += this.innerText.toLowerCase()
+				}
+			}
+		})
+	})
+}
+
+/*==============KEYPRESS============= */
+
+
+const spaceKey = document.querySelector('.space_key'),
+	shiftL = document.querySelector('.shift_left'),
+	shiftR = document.querySelector('.shift_right'),
+	caps_Lock = document.querySelector('.capslock_key'),
+	altR = document.querySelector('.alt_right'),
+	altL = document.querySelector('.alt_left'),
+	arrowL = document.querySelector('.left_arr'),
+	arrowR = document.querySelector('.right_arr'),
+	arrowUp = document.querySelector('.up'),
+	enterKey = document.querySelector('.enter_key'),
+	backSpace = document.querySelector('.backspace_key'),
+	meta = document.querySelector('.win'),
+	arrowD = document.querySelector('.down')
+
+for (let i = 0; i < button.length; i++) {
+	button[i].setAttribute('keyname', button[i].innerText)
+	button[i].setAttribute('lowerCaseName', button[i].innerText.toLowerCase())
+}
+if (button) {
+	window.addEventListener('keydown', function (e) {
+		for (let i = 0; i < button.length; i++) {
+			if (e.key == button[i].getAttribute('keyname') || e.key == button[i].getAttribute('lowerCaseName')) {
+				button[i].classList.add('active')
+				if (!button[i].classList.contains('enter_key') &&
+					!button[i].classList.contains('backspace_key') &&
+					!button[i].classList.contains('shift_left') &&
+					!button[i].classList.contains('shift_right') &&
+					!button[i].classList.contains('alt') &&
+					!button[i].classList.contains('tab_key'))
+					if (!shiftL.classList.contains('active') && !shiftR.classList.contains('active')
+						&& !caps_Lock.classList.contains('active_caps')) {
+						textInput.innerHTML += button[i].innerText.toLowerCase()
+					} else if (caps_Lock.classList.contains('active_caps') &&
+						shiftL.classList.contains('active')) {
+						textInput.innerHTML += button[i].innerText.toLowerCase()
+					} else if ((caps_Lock.classList.contains('active_caps') &&
+						shiftR.classList.contains('active'))) {
+						textInput.innerHTML += button[i].innerText.toLowerCase()
+					}
+					else {
+						textInput.innerHTML += button[i].innerText.toUpperCase()
+					}
+			}
+			e.preventDefault()
+		}
+		if (e.code) {
+			console.log(e);
+		}
+		if (e.code == 'MetaLeft') {
+			meta.classList.add('active')
+		}
+		if (e.code == 'ArrowLeft') {
+			arrowL.classList.add('active')
+			textInput.innerHTML += "&larr;"
+		}
+		/*=======================shift Left======= */
+		if (shiftL.classList.contains('active') && e.code == 'Equal') {
+			textInput.innerHTML += '+'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Minus') {
+			textInput.innerHTML += '_'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Backquote') {
+			textInput.innerHTML += '~'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit0') {
+			textInput.innerHTML += ')'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit9') {
+			textInput.innerHTML += '('
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit8') {
+			textInput.innerHTML += '*'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit7') {
+			textInput.innerHTML += '&'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit6') {
+			textInput.innerHTML += '^'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit5') {
+			textInput.innerHTML += '%'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit4') {
+			textInput.innerHTML += '$'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit3') {
+			textInput.innerHTML += '#'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit2') {
+			textInput.innerHTML += '@'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Digit1') {
+			textInput.innerHTML += '!'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'BracketLeft') {
+			textInput.innerHTML += '{'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'BracketRight') {
+			textInput.innerHTML += '}'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Semicolon') {
+			textInput.innerHTML += ':'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Quote') {
+			textInput.innerHTML += '"'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Comma') {
+			textInput.innerHTML += '<'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Slash') {
+			textInput.innerHTML += '?'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Period') {
+			textInput.innerHTML += '>'
+		}
+		if (shiftL.classList.contains('active') && e.code == 'Backslash') {
+			textInput.innerHTML += '|'
+		}
+		/*=======================shift right======= */
+		if (shiftR.classList.contains('active') && e.code == 'Equal') {
+			textInput.innerHTML += '+'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Minus') {
+			textInput.innerHTML += '_'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Backquote') {
+			textInput.innerHTML += '~'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit0') {
+			textInput.innerHTML += ')'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit9') {
+			textInput.innerHTML += '('
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit8') {
+			textInput.innerHTML += '*'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit7') {
+			textInput.innerHTML += '&'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit6') {
+			textInput.innerHTML += '^'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit5') {
+			textInput.innerHTML += '%'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit4') {
+			textInput.innerHTML += '$'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit3') {
+			textInput.innerHTML += '#'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit2') {
+			textInput.innerHTML += '@'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Digit1') {
+			textInput.innerHTML += '!'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'BracketLeft') {
+			textInput.innerHTML += '{'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'BracketRight') {
+			textInput.innerHTML += '}'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Semicolon') {
+			textInput.innerHTML += ':'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Quote') {
+			textInput.innerHTML += '"'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Comma') {
+			textInput.innerHTML += '<'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Slash') {
+			textInput.innerHTML += '?'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Period') {
+			textInput.innerHTML += '>'
+		}
+		if (shiftR.classList.contains('active') && e.code == 'Backslash') {
+			textInput.innerHTML += '|'
+		}
+		/*=============CTRLZ============= */
+		if (CtrlLeft.classList.contains('active') && e.code == 'KeyZ' ||
+			ctrlRight.classList.contains('active') && e.code == 'KeyZ') {
+			let str = textInput.innerHTML
+			textInput.innerHTML = str.substring(0, (str.length - 2))
+		}
+		if (e.code == 'ArrowRight') {
+			arrowR.classList.add('active')
+			textInput.innerHTML += "&rarr;"
+		}
+		if (e.code == 'ArrowDown') {
+			arrowD.classList.add('up_active')
+			arrowD.classList.remove('up_down')
+			textInput.innerHTML += "&darr;"
+		}
+		if (e.code == 'ArrowUp') {
+			arrowUp.classList.add('up_active')
+			arrowUp.classList.remove('up_down')
+			textInput.innerHTML += "&uarr;"
+		}
+		if (e.code == 'Tab') {
+			textInput.innerHTML += '\t'
+		}
+		if (e.code == 'Space') {
+			spaceKey.classList.add('active')
+			textInput.innerHTML += ' '
+		}
+		if (e.code == 'ShiftLeft') {
+			shiftR.classList.remove('active')
+		}
+		if (e.code == 'ShiftRight') {
+			shiftL.classList.remove('active')
+		}
+		if (e.code == 'ControlLeft') {
+			CtrlLeft.classList.add('active')
+		}
+		if (e.code == 'ControlRight') {
+			ctrlRight.classList.add('active')
+		}
+		if (e.code == 'AltLeft') {
+			altRight.classList.remove('active')
+		}
+		if (e.code == 'AltRight') {
+			altLeft.classList.remove('active')
+		}
+		if (e.code == 'Backspace') {
+			let str = textInput.innerHTML
+			textInput.innerHTML = str.substring(0, (str.length - 1))
+		}
+		if (e.code == 'CapsLock') {
+			caps_Lock.classList.toggle('active_caps')
+		}
+		if (e.code == 'Enter') {
+			textInput.innerHTML += '\n'
+		}
+	})
+	window.addEventListener('keyup', function (e) {
+		for (let i = 0; i < button.length; i++) {
+			if (e.key == button[i].getAttribute('keyname') || e.key == button[i].getAttribute('lowerCaseName')) {
+				button[i].classList.remove('active')
+				button[i].classList.add('remove')
+			}
+			if (e.code == 'MetaLeft') {
+				meta.classList.remove('active')
+			}
+			if (e.code == 'ArrowRight') {
+				arrowR.classList.remove('active')
+			}
+			if (e.code == 'ArrowLeft') {
+				arrowL.classList.remove('active')
+			}
+			if (e.code == 'ArrowUp') {
+				arrowUp.classList.remove('up_active')
+				arrowUp.classList.add('up_down')
+			}
+			if (e.code == 'ArrowDown') {
+				arrowD.classList.remove('up_active')
+				arrowD.classList.add('up_down')
+			}
+			if (e.code == 'AltLeft') {
+				altRight.classList.remove('active')
+				altRight.classList.remove('remove')
+			}
+			if (e.code == 'AltRight') {
+				altLeft.classList.remove('active')
+				altLeft.classList.remove('remove')
+			}
+			if (e.code == 'ControlLeft') {
+				ctrlRight.classList.remove('remove')
+				CtrlLeft.classList.remove('active')
+			}
+			if (e.code == 'ControlRight') {
+				CtrlLeft.classList.remove('remove')
+				ctrlRight.classList.remove('active')
+			}
+			if (e.code == 'Space') {
+				spaceKey.classList.remove('active')
+				spaceKey.classList.add('remove')
+			}
+			if (e.code == 'ShiftLeft') {
+				shiftR.classList.remove('active')
+				shiftR.classList.remove('remove')
+			}
+
+			if (e.code == 'ShiftRight') {
+				shiftL.classList.remove('active')
+				shiftL.classList.remove('remove')
+			}
+			setTimeout(() => {
+				button[i].classList.remove('remove')
+			}, 100)
+		}
+	})
+}
